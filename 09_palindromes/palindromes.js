60f6864c1301fb298870522ef2380a6e1976bb1e
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2024-12-16 15:33:35 -0800

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const palindromeCheckString = string.replace(/[\s\p{P}]/gu, '').toLowerCase();
+    const reversedString = palindromeCheckString.split('').reverse().join('');
+    return palindromeCheckString === reversedString;
 };
 
 // Do not edit below this line
