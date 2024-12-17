9c5079cbefc146780a8121882b5cd39a8f88d1e6
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2024-12-16 18:41:11 -0800

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people
+            .reduce((oldest, current) => {
+                if (!oldest.yearOfDeath) {
+                    oldest.yearOfDeath = new Date().getFullYear();
+                }
+                if (!current.yearOfDeath) {
+                    current.yearOfDeath = new Date().getFullYear();
+                }
+                const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+                const currentAge = current.yearOfDeath - current.yearOfBirth;
 
+                return oldestAge > currentAge ? oldest : current;
+            });
 };
 
 // Do not edit below this line
