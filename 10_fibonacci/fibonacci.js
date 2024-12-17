9d92c245811fa6ea7fe18f9a3e21155c4a94865e
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2024-12-16 16:32:59 -0800

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+
+    //const member = parseInt(member);
+    
+    if (parseInt(member) < 0) {
+        return "OOPS";
+    }
+
+    if (parseInt(member) === 0) {
+        return 0;
+    }
+
+    if (parseInt(member) === 1) {
+        return 1;
+    }
+
+    let fibonacciSequenceMember = 0;
+    let current = 0;
+    let next = 1;
+    
+    for (let i = 1; i < member; i++) {
+        fibonacciSequenceMember = current + next;
+        current = next;
+        next = fibonacciSequenceMember;
+    }
+
+    return fibonacciSequenceMember;
 
 };
 
